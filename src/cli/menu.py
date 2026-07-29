9d7cd340f9f6e707347f60559d33685a33fa17a5
lucasmads <lucasmads@proton.me>
2026-07-29 20:04:08 +0300

    Add menu display function for permission system

---
+++ src/cli/menu.py
@@ -0,0 +1,15 @@
+def show_menu():
+
+    print("\n" + "=" * 40)
+    print("      Permission System")
+    print("=" * 40)
+
+    print("1. List users")
+    print("2. Create user")
+    print("3. List roles")
+    print("4. Create role")
+    print("5. Check permission")
+    print("6. Statistics")
+    print("7. Exit")
+
+    print("-" * 40)
